2ebcb6a1b681bc121332af03f055c0dc27b80c55
adz3k <124911028+adz3k@users.noreply.github.com>
2023-08-22 21:12:55 +0100

    Update NewsFeed.cs resubmission
    resubmission

--- ConsoleAppProject/App04/NewsFeed.cs
+++ ConsoleAppProject/App04/NewsFeed.cs
@@ -1,76 +1,61 @@
-﻿using System;
+using ConsoleAppProject.App04;
 using System.Collections.Generic;
+using System;
 
-
-namespace ConsoleAppProject.App04
+public class NewsFeed
 {
+    public const string AUTHOR = "Armstrong";
+    private readonly List<Post> posts;
+
     ///<summary>
-    /// The NewsFeed class stores news posts for the news feed in a social network 
-    /// application.
-    /// 
-    /// Display of the posts is currently simulated by printing the details to the
-    /// terminal. (Later, this should display in a browser.)
-    /// 
-    /// This version does not save the data to disk, and it does not provide any
-    /// search or ordering functions.
+    /// Construct an empty news feed.
     ///</summary>
-    ///<author>
-    ///  Michael Kölling and David J. Barnes
-    ///  version 0.1
-    ///</author> 
-    public class NewsFeed
+    public NewsFeed()
     {
-        public const string AUTHOR = " Armstrong";
-        private readonly List<Post> posts;
+        posts = new List<Post>();
+        MessagePost messagePost = new MessagePost(AUTHOR, "I Love Manchester united ");
+        AddMessagePost(messagePost);
 
+        PhotoPost photoPost = new PhotoPost(AUTHOR, "Photo1.jpg", "Manchester united");
+        AddPhotoPost(photoPost);
+    }
 
-        ///<summary>
-        /// Construct an empty news feed.
-        ///</summary>
-        public NewsFeed()
+    ///<summary>
+    /// Add a text post to the news feed.
+    /// 
+    /// @param message The text post to be added.
+    ///</summary>
+    public void AddMessagePost(MessagePost message)
+    {
+        posts.Add(message);
+    }
+
+    ///<summary>
+    /// Add a photo post to the news feed.
+    /// 
+    /// @param photo The photo post to be added.
+    ///</summary>
+    public void AddPhotoPost(PhotoPost photo)
+    {
+        posts.Add(photo);
+    }
+
+    ///<summary>
+    /// Show the news feed. Currently: print the news feed details to the
+    /// terminal. (To do: replace this later with display in a web browser.)
+    ///</summary>
+    public void Display()
+    {
+        // display all posts
+        foreach (Post post in posts)
         {
-            posts = new List<Post>();
-            MessagePost post = new MessagePost(" Armstrong", "I Love Manchester united ");
-            AddMessagePost(post);
-
-            PhotoPost photopost = new PhotoPost(AUTHOR, "Photo1.jpg", "Manchester united");
-            AddPhotoPost(photopost);
-        }
-
-
-        ///<summary>
-        /// Add a text post to the news feed.
-        /// 
-        /// @param text  The text post to be added.
-        ///</summary>
-        public void AddMessagePost(MessagePost message)
-        {
-            messages.Add(message);
-        }
-
-        ///<summary>
-        /// Add a photo post to the news feed.
-        /// 
-        /// @param photo  The photo post to be added.
-        ///</summary>
-        public void AddPhotoPost(PhotoPost photo)
-        {
-            posts.Add(photo);
-        }
-
-        ///<summary>
-        /// Show the news feed. Currently: print the news feed details to the
-        /// terminal. (To do: replace this later with display in web browser.)
-        ///</summary>
-        public void Display()
-        {
-            // display all text posts
-            foreach (Post post in posts)
-            {
-                post.Display();
-                Console.WriteLine();   // empty line between posts
-            }
+            post.Display();
+            Console.WriteLine();   // empty line between posts
         }
     }
 
+
 }
+
+        
+
