4cce2449247c320a98bf57b34025ebbd13743a41
adz3k <124911028+adz3k@users.noreply.github.com>
2023-08-22 21:16:28 +0100

    Update Program.cs and resubmission
    resubmission

--- ConsoleAppProject/Program.cs
+++ ConsoleAppProject/Program.cs
@@ -1,29 +1,63 @@
-
-
 using ConsoleAppProject.App01;
 using ConsoleAppProject.App02;
 using ConsoleAppProject.App03;
 using ConsoleAppProject.App04;
-using ConsoleAppProject.Helpers;
 using System;
 
 namespace ConsoleAppProject
 {
-    /// <summary>
-    /// The main method in this class is called first
-    /// when the application is started.  It will be used
-    /// to start App01 to App05 for CO453 CW1
-    /// 
-    /// This Project has been modified by:
-    /// Armstrong lawal 30/3/2023
-    /// </summary>
-    public static class Program
+    public class Program
     {
-
         public static void Main(string[] args)
         {
-            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.ForegroundColor = ConsoleColor.Yellow;
 
+            Console.WriteLine();
+            Console.WriteLine(" =================================================");
+            Console.WriteLine("    BNU CO453 Applications Programming 2022-2023! by armstrong lawal");
+            Console.WriteLine(" =================================================");
+            Console.WriteLine();
+
+            Console.WriteLine("Select an option:");
+            Console.WriteLine("1. Distance Converter");
+            Console.WriteLine("2. BMI Calculator");
+            Console.WriteLine("3. Student Grades");
+            Console.WriteLine("4. Social Media");
+
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    // Run Distance Converter
+                    DistanceConverter converter = new DistanceConverter();
+                    converter.ConvertDistance();
+                    break;
+                case 2:
+                    // Run BMI Calculator
+                    BMI bmiCalculator = new BMI();
+                    bmiCalculator.Run();
+                    break;
+                case 3:
+                    // Run Student Grades
+                    StudentGrades studentGrades = new StudentGrades();
+                    studentGrades.Run();
+                    break;
+                case 4:
+                    // Run Social Media
+                    PostUI socialMedia = new PostUI();
+                    socialMedia.Run();
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice. Please select 1, 2, 3, or 4.");
+                    break;
+
+
+            }
+        }
+    }
+
+}
             Console.WriteLine();
             Console.WriteLine(" =================================================");
             Console.WriteLine("    BNU CO453 Applications Programming 2022-2023! ");
